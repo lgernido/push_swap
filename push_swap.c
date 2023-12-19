28426130a214ce618daa8f98f706660072dd66d9
Lucie <luciegernidos.pro@gmail.com>
2023-12-19 18:44:20 +0100

    over

--- push_swap.c
+++ push_swap.c
@@ -3,28 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
+/*   By: luciegernidos <luciegernidos@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 11:40:03 by lgernido          #+#    #+#             */
-/*   Updated: 2023/12/19 16:20:52 by lgernido         ###   ########.fr       */
+/*   Updated: 2023/12/19 18:28:53 by luciegernid      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+static void process_arguments(int argc, char **argv, t_stack **a, t_stack **b)
 {
-	t_stack	*a;
-	t_stack	*b;
+	bool split = false;
 
-	a = NULL;
-	b = NULL;
+	*a = NULL;
+	*b = NULL;
+
 	if (argc == 1)
-		ft_error_found(&a);
-	else if (argc == 2)
+		ft_error_found(a);
+	else if (argc == 2) 
+	{
 		argv = ft_split(argv[1], ' ');
-	ft_init_stack(&a, argv + 1);
-	if (!ft_a_sorted(a))
+		split = true;
+	}
+	ft_init_stack(a, argv + 1);
+	if (split)
+		free_split(argv);
+}
+
+static void execute_sorting(t_stack *a, t_stack *b)
+{
+	if (!ft_a_sorted(a)) 
 	{
 		if (ft_stack_size(a) == 2)
 			ft_make_sa(a);
@@ -33,7 +42,15 @@ int	main(int argc, char **argv)
 		else
 			ft_push_swap(&a, &b);
 	}
-	free_split(argv);
 	ft_stack_clear(&a);
+}
+
+int main(int argc, char **argv) 
+{
+	t_stack *a;
+	t_stack *b;
+
+	process_arguments(argc, argv, &a, &b);
+	execute_sorting(a, b);
 	return (0);
 }
